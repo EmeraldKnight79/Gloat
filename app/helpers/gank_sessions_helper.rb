edb01d638e9ecef276f74429bf0fa72222b0a214
Kyle Montag <thekylemontag@gmail.com>
2017-09-26 19:15:18 -0400

    more super cool updates

--- app/helpers/gank_sessions_helper.rb
+++ app/helpers/gank_sessions_helper.rb
@@ -1,2 +1,5 @@
 module GankSessionsHelper
+  def item_icon(item_name, count=1, quality=0)
+    "https://gameinfo.albiononline.com/api/gameinfo/items/#{item_name}.png?count=#{count}&quality=#{quality}"
+  end
 end
